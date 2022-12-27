fcdd7fb8be9dcf2da4437bc3f05d274a1c8d961d
Merlijn van der Kamp <zemanzo.my@gmail.com>
2022-12-27 23:59:36 +0100

    Update default server port

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === "development";
 
 export const API_ROOT_URL = isDev
-  ? "http://localhost:3005"
+  ? "http://localhost:3006"
   : window.location.origin; // Development
